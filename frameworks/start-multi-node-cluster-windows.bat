38488d0e0a3a7f022e683b964c6627b1330975f0
naveenpn.trainer@gmail.com <naveenpn.trainer@gmail.com>
2024-07-26 23:23:48 +0530

    Added

--- frameworks/start-multi-node-cluster-windows.bat
+++ frameworks/start-multi-node-cluster-windows.bat
@@ -6,7 +6,7 @@ echo Which containers do you want to kill and remove?
 echo 1. Login to the existing cluster (master node)
 echo 2. Create a new cluster and proceed to log in.
 echo 3. Fresh Install (Takes time)
-echo 3. Resolve ports are not available error
+echo 4. Resolve ports are not available error
 set /p choice=Enter your choice (1-3): 
 
 if "%choice%"=="1" (
@@ -17,8 +17,8 @@ if "%choice%"=="1" (
 	set images = npnnaveenpn/hadoop-slave npnnaveenpn/hadoop-master npnnaveenpn/hadoop-base
     goto compose_up	
 ) else if "%choice%"=="3" (
-    net stop winnat
-	net start winnat
+    echo "Execute commands in Administrator mode: net stop winnat"
+	echo "Execute commands in Administrator mode: net start winnat"
 ) else (
     echo Invalid choice. Please try again.
     goto menu
